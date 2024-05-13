1885e719e9b57705bf9745ba8665a6f263f689e0
Prajwal Bharadwaj BM <prajwal.bharadwaj@betsol.com>
2024-05-13 19:29:43 +0530

    add subtests and constants

--- 01-Hello,World/hello.go
+++ 01-Hello,World/hello.go
@@ -2,12 +2,28 @@ package hello
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
 // Printing Hello World Directly to stdout will be outside the domain, thus we would not be able to test this.
 /* Hence learn-go-with-tests suggest us to use a function to send in a string to Println(outside world), as we have control over the string and also achieving the goal of printing it to outside world(stdout) */
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World!"))
+	fmt.Println(Hello("World!", ""))
 }
--- 01-Hello,World/hello_test.go
+++ 01-Hello,World/hello_test.go
@@ -5,9 +5,30 @@ import (
 )
 
 func Test_withGreetName(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world' when empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("greet in spanish", func(t *testing.T) {
+		got := Hello("Darshan", "Spanish")
+		want := "Hola, Darshan"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("greet in french", func(t *testing.T) {
+		got := Hello("Aadhithya", "French")
+		want := "Bonjour, Aadhithya"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
