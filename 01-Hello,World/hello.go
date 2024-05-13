8f18135bb65487184aa24cffe2826ee35ec711eb
Prajwal Bharadwaj BM <prajwal.bharadwaj@betsol.com>
2024-05-13 19:33:58 +0530

    refactor: if statements to switch to add support for more language

--- 01-Hello,World/hello.go
+++ 01-Hello,World/hello.go
@@ -15,13 +15,14 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "world"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
+	prefix := englishHelloPrefix
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
